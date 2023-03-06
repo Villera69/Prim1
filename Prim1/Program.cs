763800745054e4a80b1bd420e324c27f56caa56a
Villera69 <william.ramsfeldt@gmail.com>
2023-03-06 21:03:28 +0100

    First iteration. Works until 11 * 11, need to think of a fix

--- Prim1/Program.cs
+++ Prim1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+Console.WriteLine("Define a positive integer 'n': ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Define a positive integer 'm' that is larger than 'n' : ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine($"Here are all of the prime numbers between {n} and {m}:");
+
+for (int i = n; i <= m; i++)
+{
+    if((i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0) || i == 2 || i == 3 || i == 5 || i == 7){
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("Press enter to leave the program.");
+Console.ReadLine();
